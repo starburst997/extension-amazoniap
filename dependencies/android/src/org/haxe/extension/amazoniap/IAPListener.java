91114358973f9e8cd7dc10ef51a0f3fe4625c605
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-05-04 16:22:41 -0400

    Compile fix

--- dependencies/android/src/org/haxe/extension/amazoniap/IAPListener.java
+++ dependencies/android/src/org/haxe/extension/amazoniap/IAPListener.java
@@ -14,36 +14,36 @@ public class IAPListener implements PurchasingListener {
 		if (Extension_iap_amazon._callback==null) {
 			return;
 		}
-		try {
+		//try {
 			Extension_iap_amazon._callback.call1 ("onProductDataResponse", productDataResponse.toString());
-		} catch (JSONException e) {}
+		//} catch (JSONException e) {}
 	}
 
 	public void onPurchaseResponse (PurchaseResponse purchaseResponse) {
 		if (Extension_iap_amazon._callback==null) {
 			return;
 		}
-		try {
+		//try {
 			Extension_iap_amazon._callback.call1("onPurchaseResponse", purchaseResponse.toString());
-		} catch (JSONException e) {}
+		//} catch (JSONException e) {}
 	}
 
 	public void onPurchaseUpdatesResponse (PurchaseUpdatesResponse purchaseUpdatesResponse) {
 		if (Extension_iap_amazon._callback==null) {
 			return;
 		}
-		try {
+		//try {
 			Extension_iap_amazon._callback.call1("onPurchaseUpdatesResponse", purchaseUpdatesResponse.toString());
-		} catch (JSONException e) {}
+		//} catch (JSONException e) {}
 	}
 
 	public void onUserDataResponse (UserDataResponse userDataResponse) {
 		if (Extension_iap_amazon._callback==null) {
 			return;
 		}
-		try {
+		//try {
 			Extension_iap_amazon._callback.call1("onUserDataResponse", userDataResponse.toString());
-		} catch (JSONException e) {}
+		//} catch (JSONException e) {}
 	}
 
 }
